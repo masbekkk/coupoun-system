273f91eef3ffcbc0eab1a39a684dee58aceb5bc7
mohammad subkhan <subkhanpinter22@gmail.com>
2026-07-14 12:34:55 +0700

    upd logo app

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -4,7 +4,7 @@ export default function AppLogoIcon(props: ImgHTMLAttributes<HTMLImageElement>) 
     return (
         <img
             {...props}
-            src="https://jasuindo.com/wp-content/themes/jasuindo-child/img/logo-jasuindo-color.png"
+            src="/logo-jasuindo-color.png"
             alt="Jasuindo Logo"
             className={`object-contain ${props.className || ''}`}
         />
